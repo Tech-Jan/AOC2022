f3cd5fc69275557e2f54d90d1633c16ae4b30014
tech-jan <trewej@hotmail.com>
2022-12-29 22:00:06 +0100

    part2 optimized. nice visualization. big calculations done

--- DAY15/src/main.py
+++ DAY15/src/main.py
@@ -1,103 +1,113 @@
-from DAY15.input.filereader import filereader
-from map import Cave,Objects,Sensor,Beacon
-from copy import deepcopy
+from map import Cave, Sensor, Beacon
 import time
+from copy import deepcopy
+
+from map import Cave, Sensor, Beacon
+
+
+def part1(cave):
+    line_number = 2000000
+
+    for sensor in Sensor.instances:
+        print(Sensor.instances.index(sensor), "/", len(Sensor.instances))
+        sensor.signalx(line_number)
+
+    beacons = set([])
+    for beacon in Beacon.instances:
+        beacons.add((beacon.coords["x"], beacon.coords["y"]))
+    set3 = cave.signals - beacons
+
+    counter2 = 0
+    for signal in set3:
+        if signal[1] == line_number:
+            counter2 += 1
+
+    print("part1", counter2)
+
+def sum_ranges(sum_ranges_line,my_sum_result):
+    for result in sum_ranges_line:
+        remove = False
+        if result[0] <= my_sum_result[0] <= result[1]:
+            my_sum_result[0] = result[0]
+            remove = True
+
+        if result[0]-1 <= my_sum_result[1] <= result[1]:
+            my_sum_result[1] = result[1]
+            remove = True
+        if my_sum_result[0] < result[0] and my_sum_result[1] > result[1]:
+            remove = True
+        if remove:
+            sum_ranges_line.remove(result)
+
+def empty_space_found(my_sum_result,sum_ranges_line,max_size,line_number):
+    my_sum_result.append(sum_ranges_line)
+    print(my_sum_result)
+    print(sum_ranges_line)
+    res = 0
+    if my_sum_result[0] == 0:
+        print("x is the missing number, in this case x=", my_sum_result[1] + 1)
+        res = my_sum_result[1] + 1
+    if my_sum_result[1] == max_size:
+        print("x is the missing number, in this case x=", my_sum_result[0] - 1)
+        res = my_sum_result[0] - 1
+    print("this is y", line_number)
+    res = res * 40000000 + line_number
+    print("resultpar2=", res)
+    exit()
+    found = True
+
+def range_sensor_equilizer(sum_ranges_line:list,max_size:int,line_number:int):
+    my_sum_result = sum_ranges_line[0]
+    sum_ranges_line.pop(0)
+    t = 0
+    found= False
+    while len(sum_ranges_line) > 0 and not found:
+        t += 1
+        if my_sum_result[0] == 0 and my_sum_result[1 == max_size]:
+            break
+        sum_ranges(sum_ranges_line,my_sum_result)
+        if t == 100:
+            found=True
+            empty_space_found(my_sum_result,sum_ranges_line,max_size,line_number)
+            break
+
+def part2():
+    found = False
+    max_size = 4000000
+    for line_number in range(0, max_size):
+        if line_number%1000==0:
+            print(round(line_number/max_size*100,1),"/100 percent scanned")
+        sum_ranges_line = []
+        for my_sensor in Sensor.instances:
+            range_sensor_for_line = my_sensor.signal_part2(line_number,max_size)
+            if range_sensor_for_line[0]:
+                sum_ranges_line.append(range_sensor_for_line[1])
+                if range_sensor_for_line[1][0] == 0 and range_sensor_for_line[1][1] == max_size:
+                    break
+
+        range_sensor_equilizer(sum_ranges_line, max_size,line_number)
+
+
+
+    # for my_sensor in Sensor.instances:
+    #     my_sensor.signalx(y)
+    # print(max(list(cave.signals),key=lambda obj:obj[0])[0])
+    # print(min(list(cave.signals), key=lambda obj: obj[0])[0])
+
 
 def main():
     # part1
-    y=2000000
-    cave=Cave()
-    for my_sensor in Sensor.instances:
-        my_sensor.distances()
-    for sensor in Sensor.instances:
-        print (sensor.distance)
-        sensor.signalx(y)
-    beacons=set([])
-    for beacon in Beacon.instances:
-        beacons.add((beacon.coords["x"],beacon.coords["y"]))
-    set3=cave.signals-beacons
-    counter2=0
-    for signal in set3:
-        if signal[1]==y:
-            counter2+=1
-    print("part1",counter2)
-
+    cave = Cave()
+    part1(cave)
     time.sleep(5)
 
-    #part2
-    found=False
-
-    for my_sensor in Sensor.instances:
-        my_sensor.distances()
-
-    for y in range(0,4000000):
-
-        print(y)
-        if found==True:
-
-            exit()
-
-        my_result = []
-        st=time.time()
-        for my_sensor in Sensor.instances:
-            my_result2=my_sensor.signal_part2(y)
-            if my_result2[0]:
-                my_result.append(my_result2[1])
-                if my_result2[1][0]==0 and my_result2[1][1]==4000000:
-                    break
-        my_sum_result=my_result[0]
-        my_result.pop(0)
-        t = 0
-        et=time.time()
-        st2=time.time()
-        while len(my_result)>0:
-            t+=1
-            if t==100:
-                my_sum_result.append(deepcopy(my_result))
-                print(my_sum_result)
-                print(my_result)
-                res=0
-                if my_sum_result[0] ==0:
-                    print("x is the missing number, in this case x=", my_sum_result[1]+1 )
-                    res=my_sum_result[1]+1
-                if my_sum_result[1] ==4000000:
-                    print("x is the missing number, in this case x=", my_sum_result[0]-1 )
-                    res=my_sum_result[0]-1
-                print("this is y",y)
-                res=res*4000000+y
-                print(res)
-                found=True
-            if my_sum_result[0]==0 and my_sum_result[1==4000000]:
-                break
-            for result in my_result:
-                remove = False
-                if result[0]<=my_sum_result[0]<=result[1]:
-                    my_sum_result[0]=result[0]
-                    remove=True
-
-                if result[0] <= my_sum_result[1] <= result[1]:
-                    my_sum_result[1] = result[1]
-                    remove=True
-                if my_sum_result[0]<result[0] and my_sum_result[1]>result[1]:
-                    remove = True
-
-                if remove:
-                    my_result.remove(result)
-
-        et2=time.time()
+    # part2
+    part2()
 
 
-
-        # for my_sensor in Sensor.instances:
-        #     my_sensor.signalx(y)
-        # print(max(list(cave.signals),key=lambda obj:obj[0])[0])
-        # print(min(list(cave.signals), key=lambda obj: obj[0])[0])
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-    #13071210703981 to high
-    #13071210703981
-    #13071206703981
-    #130712086703981
+    # 13071210703981 to high
+    # 13071210703981
+    # 13071206703981
+    # 130712086703981
--- DAY15/src/map.py
+++ DAY15/src/map.py
@@ -3,6 +3,9 @@ import pandas as pd
 from DAY15.input.filereader import filereader
 
 FILE = "../input/input"
+#in main
+#change maxsize to 4000000 if you chose input
+#change maxsize to 20 if you chose testinput
 
 
 class Cave:
@@ -11,16 +14,17 @@ class Cave:
         self.objects = []
         self.datareader()
         self.map_size = {}
-
         self.dataframe = None
+        self.signals = set([])
+        self.initialize()
 
-        self.signals=set([])
+    def initialize(self):
+        for object in self.objects:
+            object.distances()
 
-
-
-    def paint_map(self,signals:list):
+    def paint_map(self, signals: list):
         for signal in signals:
-            self.dataframe[signal[0]][signal[1]]="#"
+            self.dataframe[signal[0]][signal[1]] = "#"
 
     def dataframe_creator(self):
         if self.map_size["max_x"] < 1000:
@@ -32,8 +36,8 @@ class Cave:
         for sensorbeacon in self.data:
             sensor = sensorbeacon[0]
             beacon = sensorbeacon[1]
-            sensor = Sensor(sensor[0], sensor[1],self)
-            beacon = Beacon(beacon[0], beacon[1],self)
+            sensor = Sensor(sensor[0], sensor[1], self)
+            beacon = Beacon(beacon[0], beacon[1], self)
             sensor.closest_beacon = beacon
             beacon.closest_sensor = sensor
             self.objects.append(sensor)
@@ -56,14 +60,11 @@ class Cave:
         self.map_size = mapsize
 
 
-
-
-
 class Objects:
 
-    def __init__(self, x: int, y: int, cave:Cave):
+    def __init__(self, x: int, y: int, cave: Cave):
         self.coords = {"x": x, "y": y}
-        self.cave=cave
+        self.cave = cave
 
 
 class Sensor(Objects):
@@ -74,94 +75,86 @@ class Sensor(Objects):
         self.name = "Sensor"
         self.closest_beacon = None
         self.__class__.instances.append(self)
-        self.distance=0
-
-
+        self.distance = 0
 
     def distances(self):
         distance_x = abs(self.closest_beacon.coords["x"] - self.coords["x"])
         distance_y = abs(self.closest_beacon.coords["y"] - self.coords["y"])
-        self.distance= distance_x + distance_y
+        self.distance = distance_x + distance_y
 
     @property
     def signal(self):
-        linex=20
-        liney=20
+        linex = 20
+        liney = 20
         signals = set([])
         print("NEXT")
         signals.add((self.coords["x"], self.coords["y"]))
-        for b in range(0, self.distance+1):
+        for b in range(0, self.distance + 1):
             print(len(signals))
-            for j in range(0,self.distance+1-b):
+            for j in range(0, self.distance + 1 - b):
                 signalx = self.coords["x"] + b
                 signaly = self.coords["y"] + j
-                if self.cave.map_size["max_x"]>=signalx>=self.cave.map_size["min_x"] and self.cave.map_size[
-                    "max_y"]>=signaly>=self.cave.map_size["min_y"]:
+                if self.cave.map_size["max_x"] >= signalx >= self.cave.map_size["min_x"] and self.cave.map_size[
+                    "max_y"] >= signaly >= self.cave.map_size["min_y"]:
                     signals.add((signalx, signaly))
                 signalx = self.coords["x"] - b
                 signaly = self.coords["y"] + j
-                if self.cave.map_size["max_x"]>=signalx>=self.cave.map_size["min_x"] and self.cave.map_size[
-                    "max_y"]>=signaly>=self.cave.map_size["min_y"]:
+                if self.cave.map_size["max_x"] >= signalx >= self.cave.map_size["min_x"] and self.cave.map_size[
+                    "max_y"] >= signaly >= self.cave.map_size["min_y"]:
                     signals.add((signalx, signaly))
                 signalx = self.coords["x"] + b
                 signaly = self.coords["y"] - j
-                if self.cave.map_size["max_x"]>=signalx>=self.cave.map_size["min_x"] and self.cave.map_size[
-                    "max_y"]>=signaly>=self.cave.map_size["min_y"]:
+                if self.cave.map_size["max_x"] >= signalx >= self.cave.map_size["min_x"] and self.cave.map_size[
+                    "max_y"] >= signaly >= self.cave.map_size["min_y"]:
                     signals.add((signalx, signaly))
-                signalx = self.coords["x"] -b
+                signalx = self.coords["x"] - b
                 signaly = self.coords["y"] - j
-                if self.cave.map_size["max_x"]>=signalx>=self.cave.map_size["min_x"] and self.cave.map_size[
-                    "max_y"]>=signaly>=self.cave.map_size["min_y"]:
+                if self.cave.map_size["max_x"] >= signalx >= self.cave.map_size["min_x"] and self.cave.map_size[
+                    "max_y"] >= signaly >= self.cave.map_size["min_y"]:
                     signals.add((signalx, signaly))
         for signal in signals:
             self.cave.signals.add(signal)
         return set(signals)
 
+    def signal_part2(self, line: int, max_size) -> list:
 
-    def signal_part2(self,line:int) -> list:
+        if abs(self.coords["y"] - line) <= abs(self.distance):
+            myrange = abs(self.distance - abs(self.coords["y"] - line))
 
+            start = self.coords["x"] - (myrange) + 1
+            end = self.coords["x"] + myrange
 
-        if abs(self.coords["y"]-line)<=abs(self.distance):
-            myrange=abs(self.distance-abs(self.coords["y"]-line))
-
-            start=self.coords["x"]-(myrange)+1
-            end= self.coords["x"]+myrange
-
-            start-=1
-            if start < 0 and end >0:
-                start=0
-            if start < 4000000 and end >4000000:
-                end=4000000
-            my_range=[start,end]
-            return [True,my_range]
+            start -= 1
+            if start < 0 and end > 0:
+                start = 0
+            if start < max_size and end > max_size:
+                end = max_size
+            my_range = [start, end]
+            return [True, my_range]
         else:
-            return [False,[0,0]]
+            return [False, [0, 0]]
 
-
-
-
-    def signalx(self,line):
+    def signalx(self, line):
 
         objectx = self.coords["x"]
         objecty = self.coords["y"]
         signals = set([])
 
-
-        if self.coords["y"]>line:
-            for b in range(0, self.distance+1):
-                if self.coords["y"]-b==line:
-                    for j in range(0,self.distance+1-b):
+        if self.coords["y"] > line:
+            for b in range(0, self.distance + 1):
+                if self.coords["y"] - b == line:
+                    for j in range(0, self.distance + 1 - b):
                         signalx = self.coords["x"] + j
                         signaly = self.coords["y"] - b
                         signals.add((signalx, signaly))
                         signalx = self.coords["x"] - j
                         signaly = self.coords["y"] - b
                         signals.add((signalx, signaly))
-        elif self.coords["y"]<=line:
-            for b in range(0, self.distance+1):
-                if self.coords["y"]+b==line:
+        elif self.coords["y"] <= line:
+            for b in range(0, self.distance + 1):
+                if self.coords["y"] + b == line:
 
-                    for j in range(0,self.distance+1-b):
+                    for j in range(0, self.distance + 1 - b):
                         signalx = self.coords["x"] + j
                         signaly = self.coords["y"] + b
                         signals.add((signalx, signaly))
@@ -171,9 +164,6 @@ class Sensor(Objects):
         for signal in signals:
             self.cave.signals.add(signal)
         return set(signals)
-
-
-
 
 
 class Beacon(Objects):
@@ -184,9 +174,9 @@ class Beacon(Objects):
         self.name = "Beacon"
         self.closest_sensor = None
         self.__class__.instances.append(self)
+        self.distance = 0
 
-    @property
-    def distance(self):
+    def distances(self):
         distance_x = abs(self.closest_sensor.coords["x"] - self.coords["x"])
         distance_y = abs(self.closest_sensor.coords["y"] - self.coords["y"])
-        return distance_x + distance_y
+        self.distance = distance_x + distance_y
